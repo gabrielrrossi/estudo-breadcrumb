1ff5d58fba9391aad1810aa9d7fcb63673ad31cd
Gabriel Rossi <gabrielrrossi@gmail.com>
2020-01-30 13:13:00 -0300

    Tutorial sobre Breadcrumb

---
+++ database/factories/ContinentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Continent;
+use Faker\Generator as Faker;
+
+$factory->define(Continent::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      factory(App\City::class)->create([
+        'name' => 'Johannesburg',
+        'country_id' => function(){
+
+            return factory(App\Country::class)->create([
+                'name' => 'South Africa',
+                'continent_id' => function(){
+
+                    return factory(App\Continent::class)->create([
+                        'name' => 'Africa' ])->id;
+                }
+                ])->id;
+        },
+        'name' => 'Paris',
+        'country_id' => function(){
+
+            return factory(App\Country::class)->create([
+                'name' => 'França',
+                'continent_id' => function(){
+
+                    return factory(App\Continent::class)->create([
+                        'name' => 'Europa' ])->id;
+                }
+                ])->id;
+        }
+        ]);
+    }
+}
